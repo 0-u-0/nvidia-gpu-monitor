b0239f26a607843f46c45d36a7c628dd65423e5c
Chen Cong <chencong@100doc.com.cn>
2022-06-24 09:24:53 +0800

    update test

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ console.log(`Gpu: `,gpu.isSupported());
 console.log('Device count: ',gpu.getDeviceCount());
 
 let ii = 0;
-const loop = 3;
+const loop = 10;
 const timer = setInterval(function(){
   for (let index = 0; index < gpu.getDeviceCount(); index++) {
     console.log('------------');
@@ -18,9 +18,9 @@ const timer = setInterval(function(){
     console.log('Enc: ',gpu.getEncoderUtilization(index));
     console.log('Dec: ',gpu.getDecoderUtilization(index));
     console.log('Temperatur: ',gpu.getTemperature(index));//degrees C
-    if(ii++ == loop){
-      clearInterval(timer);
-      gpu.close();
-    }  
   }
+  if(ii++ == loop){
+    clearInterval(timer);
+    gpu.close();
+  } 
 },2000);
